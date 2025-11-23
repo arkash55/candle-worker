b4b9827791cd2c3dc7c17f1b32805314281f53dd
arkash55 <arkash.vijay55@gmail.com>
2025-11-23 03:24:11 +0000

    created trade model

---
+++ app/core/models/Candle_1s.py
@@ -0,0 +1,16 @@
+
+
+
+class Candle_1s():
+    
+    @classmethod
+    def from_trades(cls, symbol, trades):
+        pass
+
+    @classmethod
+    def start_new(cls, symbol, timestamp):
+        # first trade update will be open
+        pass
+
+    def update(self, trade):
+        pass

aa724f9869f7e03f8bc3dbad91ecf28abf5646ac
arkash55 <arkash.vijay55@gmail.com>
2025-11-24 03:03:41 +0000

    added empty trades test

--- tests/unit/test_candle_1s.py
+++ tests/unit/test_candle_1s.py
@@ -1,5 +1,5 @@
 import unittest
-from datetime import datetime
+from datetime import datetime, timedelta
 from app.core.models import Candle_1s, Trade
 
 
@@ -13,27 +13,33 @@ class Test_Candle_1s(unittest.TestCase):
 
     symbol = 'AAPL'
 
-    def generate_trades(self, open, close, low, high):
+    def generate_trades(self, open_, high_, low_, close_):
         trades = []
-        sz = 5;
-        trades.append(Trade(self.symbol, open, sz, datetime.now()))
-        trades.append(Trade(self.symbol, low, sz, datetime.now()))
-        trades.append(Trade(self.symbol, high, sz, datetime.now()))
-        trades.append(Trade(self.symbol, close, sz, datetime.now()))
+        sz = 5
+        curr_time = datetime.now()
+        trades.append(Trade(self.symbol, high_, sz, curr_time + timedelta(milliseconds=20)))
+        trades.append(Trade(self.symbol, close_, sz, curr_time + timedelta(milliseconds=60)))
+        trades.append(Trade(self.symbol, open_, sz, curr_time))
+        trades.append(Trade(self.symbol, low_, sz, curr_time + timedelta(milliseconds=40)))
         return trades;
 
 
         
 
-    def test_from_trades(self):
-        open, close, low, high = 5, 10, 4, 12
-        trades = self.generate_trades(open=open, close=close, low=low, high=high)
+    def test_valid_from_trades(self):
+        open_, close_, low_, high_ = 5, 10, 4, 12
+        trades = self.generate_trades(open_=open_, high_=high_, close_=close_, low_=low_)
         candle = Candle_1s.from_trades(self.symbol, trades)
-        self.assertEqual(candle.open, open)
-        self.assertEqual(candle.close, close)
-        self.assertEqual(candle.low, low)
-        self.assertEqual(candle.high, high)
+        self.assertEqual(candle.open, open_)
+        self.assertEqual(candle.close, close_)
+        self.assertEqual(candle.low, low_)
+        self.assertEqual(candle.high, high_)
 
+
+    def test_empty_from_trades(self):
+        with self.assertRaises(ValueError):
+            Candle_1s.from_trades(self.symbol, [])
+        
 
 
 

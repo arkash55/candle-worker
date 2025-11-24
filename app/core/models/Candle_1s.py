1b9b525b69ac47f5803a40031da8eca6136415dd
arkash55 <arkash.vijay55@gmail.com>
2025-11-24 02:33:20 +0000

    add from_trades

--- app/core/models/Candle_1s.py
+++ app/core/models/Candle_1s.py
@@ -2,15 +2,70 @@
 
 
 class Candle_1s():
+
+    
+
+    def __init__(self, open, close, high, low):
+        self.__open = open
+        self.__close = close
+        self.__high = high
+        self.__low = low
+
+    # PROPERTIES
+    @property
+    def open(self):
+        return self.__open
+
+    @property
+    def close(self):
+        return self.__close
+    
+
+    @property
+    def high(self):
+        return self.__high
+
+    @property
+    def low(self):
+        return self.__low
+    
+
+    #static methods
+    def get_ohlc(trades):
+        if len(trades) == 0:
+            return None
+        
+        sorted_trades = sorted(trades, key=lambda t: t.timestamp)
+        open, close = sorted_trades[0].price, sorted_trades[-1].price
+        high = max(t.price for t in sorted_trades)
+        low = min(t.price for t in sorted_trades)
+
+        return open, high, low, close
+        
+
+        
+
+    # METHODS
+
     
     @classmethod
     def from_trades(cls, symbol, trades):
-        return True
+        # we need to get open, close, high low
+        open, high, low, close = cls.get_ohlc(trades)
+        candle = Candle_1s(open=open, close=close, high=high, low=low)
+        return candle
+
+
+
+
+
+
 
     @classmethod
     def start_new(cls, symbol, timestamp):
         # first trade update will be open
         pass
+
 
     def update(self, trade):
         pass 
--- tests/unit/test_candle_1s.py
+++ tests/unit/test_candle_1s.py
@@ -1,6 +1,6 @@
 import unittest
-
-from app.core.models import Candle_1s
+from datetime import datetime
+from app.core.models import Candle_1s, Trade
 
 
 class Test_Candle_1s(unittest.TestCase):
@@ -10,9 +10,32 @@ class Test_Candle_1s(unittest.TestCase):
     # def test_upper(self):
         # self.assertEqual('foo'.upper(), 'FOO')
 
+
+    symbol = 'AAPL'
+
+    def generate_trades(self, open, close, low, high):
+        trades = []
+        sz = 5;
+        trades.append(Trade(self.symbol, open, sz, datetime.now()))
+        trades.append(Trade(self.symbol, low, sz, datetime.now()))
+        trades.append(Trade(self.symbol, high, sz, datetime.now()))
+        trades.append(Trade(self.symbol, close, sz, datetime.now()))
+        return trades;
+
+
+        
+
     def test_from_trades(self):
-        candle = Candle_1s.from_trades('abs', 4)
-        self.assertTrue(candle)
+        open, close, low, high = 5, 10, 4, 12
+        trades = self.generate_trades(open=open, close=close, low=low, high=high)
+        candle = Candle_1s.from_trades(self.symbol, trades)
+        self.assertEqual(candle.open, open)
+        self.assertEqual(candle.close, close)
+        self.assertEqual(candle.low, low)
+        self.assertEqual(candle.high, high)
+
+
+
 
 
 
